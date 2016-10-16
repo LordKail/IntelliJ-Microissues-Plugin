fc04d833e4a8f0f5973b3549c2ea049401f7659a
LordKail <Al3xleet@hotmail.com>
2016-10-17 01:25:46 +0200

    Adds the modifications created for creating a basic HelloWorld ToolWindow.

---
+++ src/SetupToolWindow.java
@@ -0,0 +1,67 @@
+import com.intellij.openapi.actionSystem.*;
+import com.intellij.openapi.components.ProjectComponent;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.project.ProjectManager;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowAnchor;
+import com.intellij.openapi.wm.ToolWindowManager;
+import com.intellij.ui.content.ContentFactory;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+import java.awt.*;
+
+
+public class SetupToolWindow implements ProjectComponent{
+
+    private JComponent microissuesContainer;
+    private String toolwindowTitle = "Microissues";
+    private Project project;
+
+    public SetupToolWindow(Project project) {
+        this.project = project;
+    }
+
+    @Override
+    public void initComponent() {
+        //Initialising the component. Might move the methods from "projectOpened" to here.
+    }
+
+    @Override
+    public void projectOpened() {
+
+        microissuesContainer = new JPanel(new BorderLayout(1, 1));
+        microissuesContainer.setBorder(null);
+
+        ToolWindow tasksToolWindow = ToolWindowManager.getInstance(project).registerToolWindow(toolwindowTitle, false, ToolWindowAnchor.BOTTOM);
+        tasksToolWindow.getContentManager().addContent(ContentFactory.SERVICE.getInstance().createContent(microissuesContainer, "Microissues", true));
+
+        ActionGroup actionGroup = (ActionGroup) ActionManager.getInstance().getAction("TasksActionGroup");
+        ActionToolbar toolBar = ActionManager.getInstance().createActionToolbar("TasksActionGroupPlace", actionGroup, false);
+
+        ActionGroup additionalActionGroup = (ActionGroup) ActionManager.getInstance().getAction("TasksAdditionalToolBarGroup");
+        ActionToolbar additionalToolbar = ActionManager.getInstance().createActionToolbar("TasksActionGroupPlace", additionalActionGroup, false);
+
+        JPanel toolBarPanel = new JPanel(new BorderLayout(1, 1));
+        toolBarPanel.add(toolBar.getComponent(), BorderLayout.WEST);
+        toolBarPanel.add(additionalToolbar.getComponent(), BorderLayout.CENTER);
+        toolBarPanel.setBorder(null);
+    }
+
+    @Override
+    public void projectClosed() {
+        ToolWindowManager toolWindowManager = ToolWindowManager.getInstance(project);
+        toolWindowManager.unregisterToolWindow(toolwindowTitle);
+    }
+
+    @Override
+    public void disposeComponent() {
+        //Not sure what goes here.
+    }
+
+    @NotNull
+    @Override
+    public String getComponentName() {
+        return "Microissues ToolWindow Component";
+    }
+}
