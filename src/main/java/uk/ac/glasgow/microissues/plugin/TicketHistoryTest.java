08311c5bfd881a306890e991beb5fe21d23508be
LordKail <Al3xleet@hotmail.com>
2016-12-04 21:13:46 +0100

    Adds a class for testing tickets, in particular ticket history tracking through vcs. Has no innate functionality, just tickets.

---
+++ src/main/java/uk/ac/glasgow/microissues/plugin/TicketHistoryTest.java
@@ -0,0 +1,17 @@
+package uk.ac.glasgow.microissues.plugin;
+
+import org.eclipse.jgit.api.Git;
+
+import java.util.HashMap;
+
+/**
+ * Created by Al3x on 03/12/2016.
+ */
+
+public class TicketHistoryTest {
+
+    /*
+    @tckt Ticket summary
+    @type TASK
+     */
+}
