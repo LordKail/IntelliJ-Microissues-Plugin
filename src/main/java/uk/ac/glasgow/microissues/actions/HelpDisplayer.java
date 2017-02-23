69c8188c0c6f0fa439cfabf979812b131dabadf8
LordKail <Al3xleet@hotmail.com>
2017-02-23 12:07:18 +0000

    Modifies the helper window display the text in a better format and makes the GitHub URL clickable

--- src/main/java/uk/ac/glasgow/microissues/actions/HelpDisplayer.java
+++ src/main/java/uk/ac/glasgow/microissues/actions/HelpDisplayer.java
@@ -6,6 +6,9 @@ import com.intellij.openapi.actionSystem.DataKeys;
 import com.intellij.openapi.project.Project;
 
 import javax.swing.*;
+import javax.swing.event.HyperlinkEvent;
+import javax.swing.event.HyperlinkListener;
+import java.awt.*;
 
 /**
  * Class extending AnAction responsible for displaying helpful information relating to how to use the plugin and which
@@ -29,10 +32,39 @@ public class HelpDisplayer extends AnAction {
         sb.append("<p>@priority Ticket priority here [INTEGER] [OPTIONAL] \t</p>");
         sb.append("<p>*/<p>");
         sb.append("<br><br>");
+        sb.append("<p><b>If your tickets are not recognised or displayed in the Tool Window, please hit the refresh button.</b></p>");
         sb.append("<p>Further information, with examples, are available on the GitHub page of the plugin:</p>");
         sb.append("<a href='https://github.com/LordKail/IntelliJ-Microissues-Plugin'>IntelliJ Microissues Plugin</a>");
         sb.append("</html>");
-        JOptionPane.showMessageDialog(null, sb);
-    }
 
+        JEditorPane helpMessage = new JEditorPane("text/html", sb.toString());
+
+        JLabel label = new JLabel();
+
+        helpMessage.addHyperlinkListener(new HyperlinkListener()
+        {
+            @Override
+            public void hyperlinkUpdate(HyperlinkEvent e)
+            {
+                if (e.getEventType().equals(HyperlinkEvent.EventType.ACTIVATED))
+                    //ProcessHandler.launchUrl(e.getURL().toString()); // roll your own link launcher or use Desktop if J6+
+                    try
+                    {
+                        Desktop.getDesktop().browse(e.getURL().toURI());
+                    }
+                    catch(java.net.URISyntaxException e1)
+                    {
+                        System.err.println(e1.getMessage());
+                    }
+                    catch(java.io.IOException e2)
+                    {
+                        System.err.println(e2.getMessage());
+                    }
+            }
+        });
+        helpMessage.setEditable(false);
+        helpMessage.setBackground(label.getBackground());
+        JOptionPane.showMessageDialog(null, helpMessage);
+
+    }
 }
